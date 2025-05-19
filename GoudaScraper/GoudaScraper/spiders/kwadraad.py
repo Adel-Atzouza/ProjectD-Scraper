37ccfec5c303e7cc9e7e89b772d83877572303c6
nhojW <161321640+nhojW@users.noreply.github.com>
2025-05-19 23:46:03 +0200

    Update kwadraad.py
    initialisaiton van kwadraad site met hun activiteiten

--- GoudaScraper/GoudaScraper/spiders/kwadraad.py
+++ GoudaScraper/GoudaScraper/spiders/kwadraad.py
@@ -5,3 +5,9 @@ import csv
 class KwadraadKalenderSpider(scrapy.Spider):
     name = 'kwadraad_kalender'
     start_urls = ['https://www.kwadraad.nl/kalender/']
+
+    def __init__(self):
+        self.csv_file = open('kwadraad_activiteiten.csv', mode='w', newline='', encoding='utf-8')
+        fieldnames = ['Title', 'DateTime', 'Gemeente', 'Link']
+        self.writer = csv.DictWriter(self.csv_file, fieldnames=fieldnames)
+        self.writer.writeheader()
