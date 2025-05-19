a22ea2d92ad61cce709568366d1344be7d431efb
nhojW <161321640+nhojW@users.noreply.github.com>
2025-05-19 23:47:19 +0200

    Update kwadraad.py

--- GoudaScraper/GoudaScraper/spiders/kwadraad.py
+++ GoudaScraper/GoudaScraper/spiders/kwadraad.py
@@ -11,3 +11,27 @@ class KwadraadKalenderSpider(scrapy.Spider):
         fieldnames = ['Title', 'DateTime', 'Gemeente', 'Link']
         self.writer = csv.DictWriter(self.csv_file, fieldnames=fieldnames)
         self.writer.writeheader()
+
+    def parse(self, response):
+        activities = response.css("article.archive-item")
+
+        for item in activities:
+            title = item.css("h2::text").get()
+            date_time = item.css("span.date time::attr(datetime)").get()
+            gemeente = item.css("span.date::text").re_first(r"→\s*(.+)")
+            link = item.css("a.block-link::attr(href)").get()
+
+            if title and date_time and gemeente and link:
+                self.writer.writerow({
+                    'Title': title.strip(),
+                    'DateTime': date_time.strip(),
+                    'Gemeente': gemeente.strip(),
+                    'Link': link.strip()
+                })
+
+        next_page = response.css("span.prev a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def closed(self, reason):
+        self.csv_file.close()
